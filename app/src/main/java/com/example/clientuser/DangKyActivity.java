f27f0e59780d69f362ebe7fcd17f238a7eac0aaa
nguyenvanan1999 <sieutromkitin@gmail.com>
2019-09-28 11:10:07 +0700

    activity đăng ký và đăng nhập

--- app/src/main/java/com/example/clientuser/DangKyActivity.java
+++ app/src/main/java/com/example/clientuser/DangKyActivity.java
@@ -2,13 +2,29 @@ package com.example.clientuser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DangKyActivity extends AppCompatActivity {
+
+    TextView txtNhapDeDK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ky);
+
+        txtNhapDeDK = findViewById(R.id.txtNhapDeDK);
+
+        txtNhapDeDK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DangKyActivity.this, DangNhapActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/clientuser/DangNhapActivity.java
+++ app/src/main/java/com/example/clientuser/DangNhapActivity.java
@@ -2,13 +2,30 @@ package com.example.clientuser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DangNhapActivity extends AppCompatActivity {
+
+    Button btnDangNhap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
+
+        btnDangNhap = findViewById(R.id.btnDangNhap);
+
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DangNhapActivity.this, DangKyActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
