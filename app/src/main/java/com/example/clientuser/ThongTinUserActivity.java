9089f8b19e3d73caa65d6847506ef4fcfe7d3d3b
phuunguyen <phunguyen9129@gmail.com>
2019-09-28 08:15:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/clientuser/ThongTinUserActivity.java
@@ -0,0 +1,14 @@
+package com.example.clientuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThongTinUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thong_tin_user);
+    }
+}
