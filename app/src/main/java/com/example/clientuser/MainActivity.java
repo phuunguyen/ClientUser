a1e9df5a5b048e536ad9cad578b762eac8760256
NguyenSon2310 <thaison5qt@gmail.com>
2019-10-01 08:08:25 +0700

    Tạo các object cho database Tạo ID theo dạng note tăng dần và lưu biến lớn nhất của id vào trong firebase

--- app/src/main/java/com/example/clientuser/MainActivity.java
+++ app/src/main/java/com/example/clientuser/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.clientuser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,23 +8,44 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.clientuser.database.create.CreateDatabase;
 import com.example.clientuser.database.object.Delivery;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    CreateDatabase cd = new CreateDatabase();
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference mData;
     Button btnSave;
     EditText edtTime;
     int i = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mData = cd.database.getReference();
+        mData = database.getReference();
         setControl();
         setEvent();
+        mData.child("Test").child("max").addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() == null) {
+                    i = 0;
+                } else {
+                    i = Integer.parseInt(dataSnapshot.getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void setEvent() {
@@ -36,13 +58,14 @@ public class MainActivity extends AppCompatActivity {
                 delivery.setId_Shipper("SP" + i);
                 delivery.setTime_Delivery(edtTime.getText().toString());
                 mData.child("Test").child("GH" + i).setValue(delivery);
+                mData.child("Test").child("max").setValue(i);
             }
         });
 
     }
 
     private void setControl() {
-        edtTime = (EditText)findViewById(R.id.edtTime);
+        edtTime = (EditText) findViewById(R.id.edtTime);
         btnSave = (Button) findViewById(R.id.btnSave);
     }
 
---
+++ app/src/main/java/com/example/clientuser/database/object/Store.java
@@ -0,0 +1,70 @@
+package com.example.clientuser.database.object;
+
+public class Store {
+    String id_Store;
+    String store_Name;
+    String address;
+    int phone;
+    String userName;
+    String password;
+
+    public Store() {
+    }
+
+    public Store(String id_Store, String store_Name, String address, int phone, String userName, String password) {
+        this.id_Store = id_Store;
+        this.store_Name = store_Name;
+        this.address = address;
+        this.phone = phone;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getId_Store() {
+        return id_Store;
+    }
+
+    public void setId_Store(String id_Store) {
+        this.id_Store = id_Store;
+    }
+
+    public String getStore_Name() {
+        return store_Name;
+    }
+
+    public void setStore_Name(String store_Name) {
+        this.store_Name = store_Name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
