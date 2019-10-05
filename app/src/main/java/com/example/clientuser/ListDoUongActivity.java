a0bdaeb6fd04f5f36e88c3eaf2d2c7b47750b77d
locc5 <locc5152@gmail.com>
2019-10-05 09:31:48 +0700

    - load dữ liệu Product từ firebase vào list đồ uống

--- app/src/main/java/com/example/clientuser/ListDoUongActivity.java
+++ app/src/main/java/com/example/clientuser/ListDoUongActivity.java
@@ -1,27 +1,32 @@
 package com.example.clientuser;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.clientuser.Adapter.DoUongAdapter;
-import com.example.clientuser.database.object.Product;
+import com.example.clientuser.model.Product;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+//import com.example.clientuser.database.object.Product;
 
 public class ListDoUongActivity extends AppCompatActivity {
 
     private ListView lvDoUong;
-    private TextView price, tenCH;
-    private ImageView img;
-    ArrayList<Product> arrayList = new ArrayList<>();
-    ArrayAdapter<String> arrayAdapter;
-    String TAG = "FIREBASE";
+    ArrayList<Product> data = new ArrayList<>();
+    DoUongAdapter adapter = null;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,29 +34,56 @@ public class ListDoUongActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_do_uong);
 
 
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-        lvDoUong = (ListView) findViewById(R.id.productList);
+        //arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         //lvDoUong.setAdapter(arrayAdapter);
+
+        databaseReference = database.getReference();
         setControl();
         setEvent();
     }
     public void setControl(){
-        price = (TextView)findViewById(R.id.price);
-        tenCH = (TextView)findViewById(R.id.nameProduct);
-        img = (ImageView)findViewById(R.id.imgProduct);
         lvDoUong = (ListView) findViewById(R.id.productList);
     }
 
     public void setEvent(){
-        khoiTao();
-        DoUongAdapter adapter = new DoUongAdapter(this, R.layout.listview_douong,arrayList);
+        adapter = new DoUongAdapter(this, R.layout.listview_douong,data);
         lvDoUong.setAdapter(adapter);
+        loadData();
     }
 
-    public void khoiTao() {
-        arrayList.add(new Product(R.drawable.logoapp , "trà sữa siêu ngon", 25000));
-        arrayList.add(new Product(R.drawable.logoapp , "trà sữa siêu ngon", 25000));
+    public void loadData(){
+        databaseReference.child("Product").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Product product = new Product();
+                product.setImgProduct(dataSnapshot.child("Product_image").getValue().toString());
+                product.setNameProduct(dataSnapshot.child("Product_name").getValue().toString());
+                product.setPriceProduct(Double.parseDouble(dataSnapshot.child("Price").getValue().toString()));
+                data.add(product);
+                adapter.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                adapter.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
+
+
 }
