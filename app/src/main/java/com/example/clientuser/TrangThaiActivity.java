b0d7c990309121d807bfdb36536bd4192be82482
phuunguyen <phunguyen9129@gmail.com>
2019-10-05 09:12:57 +0700

    màn hình 8,9

--- app/src/main/java/com/example/clientuser/TrangThaiActivity.java
+++ app/src/main/java/com/example/clientuser/TrangThaiActivity.java
@@ -2,13 +2,53 @@ package com.example.clientuser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.clientuser.database.object.Cart;
+
+import java.util.ArrayList;
 
 public class TrangThaiActivity extends AppCompatActivity {
+    private ListView listViewTrangThai;
+    ImageButton btnDanhGia;
+    TextView txtMaDonhang;
+    ImageView imgRating, imgSP;
+    ArrayList<Cart> data = new ArrayList<>();
+    ArrayAdapter<CuaHangAdapter> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trang_thai);
+
+        setControl();
+
+        setEvent();
     }
+
+    private void setEvent() {
+        btnDanhGia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent intent = new Intent(TrangThaiActivity.this, DanhGiaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setControl() {
+        btnDanhGia = findViewById(R.id.btnDanhGia);
+        txtMaDonhang = findViewById(R.id.txtMaDonHang);
+        imgRating = findViewById(R.id.imgRating);
+        imgSP = findViewById(R.id.imgSP);
+    }
+
+
 }
