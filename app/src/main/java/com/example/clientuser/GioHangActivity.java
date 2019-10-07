d175ae820c6a3b560f2c196f8f8b583ab6fe3fac
locc5 <locc5152@gmail.com>
2019-10-07 09:19:40 +0700

    Merge remote-tracking branch 'remotes/origin/NguyenVoThaiSon' into LeThanhLiem
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/clientuser/CuaHangAdapter.java
    #	app/src/main/java/com/example/clientuser/ListCuaHangActivity.java

--- app/src/main/java/com/example/clientuser/GioHangActivity.java
+++ app/src/main/java/com/example/clientuser/GioHangActivity.java
@@ -1,10 +1,17 @@
 package com.example.clientuser;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class GioHangActivity extends AppCompatActivity {
+
+
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
