d6915b4ee63e7e9677a31a6caacad58caf21856b
NguyenNhan99 <32867603+NguyenNhan99@users.noreply.github.com>
2019-11-05 09:08:15 +0700

    Kiểm tra email đã tồn tại và không cho đăng kí

--- app/src/main/java/com/example/clientuser/DangKyActivity.java
+++ app/src/main/java/com/example/clientuser/DangKyActivity.java
@@ -1,6 +1,7 @@
 package com.example.clientuser;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -18,11 +19,13 @@ import android.widget.Toast;
 import com.example.clientuser.database.object.Users;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.regex.Matcher;
@@ -39,13 +42,14 @@ public class DangKyActivity extends AppCompatActivity {
     Users users = new Users();
     ArrayList<Users> arrUser = new ArrayList();
     int i;
+    int j = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ky);
         //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                //WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setControl();
         setEvent();
 
@@ -53,6 +57,7 @@ public class DangKyActivity extends AppCompatActivity {
     }
 
     public void setEvent() {
+        loadUser();
         mData.child("MaxID").child("MaxID_Users").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -72,30 +77,31 @@ public class DangKyActivity extends AppCompatActivity {
         btnDK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = edtEmail.getText().toString().trim();
+
+                final String email = edtEmail.getText().toString().trim();
                 String name = edtName.getText().toString().trim();
                 String adress = edtAddress.getText().toString().trim();
                 String phone = edtPhone.getText().toString().trim();
                 String pass = edtPass.getText().toString().trim();
                 String pass1 = edtPass1.getText().toString().trim();
 
-                if(!isEmptyOrNull(name)){
+                if (!isEmptyOrNull(name)) {
                     Toast.makeText(getApplicationContext(), "Vui lòng nhập tên!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(!isEmptyOrNull(adress)){
+                if (!isEmptyOrNull(adress)) {
                     Toast.makeText(getApplicationContext(), "Vui lòng nhập địa chỉ!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(!isValidEmailID(email)){
+                if (!isValidEmailID(email)) {
                     Toast.makeText(getApplicationContext(), "Email không đúng định dạng!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(!isValidPassword(pass)){
+                if (!isValidPassword(pass)) {
                     Toast.makeText(getApplicationContext(), "Mật khẩu của bạn phải từ 6 kí tự trở lên!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(!isValidPassword(pass1)){
+                if (!isValidPassword(pass1)) {
                     Toast.makeText(getApplicationContext(), "Mật khẩu nhập lại của bạn phải từ 6 kí tự trở lên!", Toast.LENGTH_SHORT).show();
                     return;
                 }
@@ -104,22 +110,35 @@ public class DangKyActivity extends AppCompatActivity {
                     return;
                 }
 
-                i++;
-                users.setName(edtName.getText().toString());
-                users.setPhone(Integer.parseInt(edtPhone.getText().toString()));
-                users.setEmail(edtEmail.getText().toString());
-                users.setAddress(edtAddress.getText().toString());
-                users.setPassword(edtPass.getText().toString());
-                users.setId_Users("User" + i);
-                if (edtPass.getText().toString().equals(edtPass1.getText().toString())) {
-                    Table_User.child("User" + i).setValue(users);
-                    Toast.makeText(getApplicationContext(), "Dang ki thanh cong", Toast.LENGTH_SHORT).show();
 
-                } else {
-                    Toast.makeText(getApplicationContext(), "Sai mat khau vui long nhap lai", Toast.LENGTH_SHORT).show();
+                if(checkEmailExist(email)){
+                    i++;
+                    dangKi(i);
                 }
-                mData.child("MaxID").child("MaxID_Users").setValue(i);
 
+
+
+//                mData.child("Users").addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                        loadUser();
+//                        for (DataSnapshot ds : dataSnapshot.getChildren())
+//                            if (ds.child("email").getValue().toString().equals(email)) {
+//                                Toast.makeText(DangKyActivity.this, "Email nay da ton tai!!", Toast.LENGTH_SHORT).show();
+//                                break;
+//                            }else{
+//                                Toast.makeText(DangKyActivity.this, "Dang ki thanh cong", Toast.LENGTH_SHORT).show();
+//                                break;
+//                            }
+//
+//
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                    }
+//                });
 
             }
         });
@@ -130,6 +149,34 @@ public class DangKyActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private boolean checkEmailExist(String email) {
+        for (int j = 0; j < arrUser.size(); j++) {
+            if (arrUser.get(j).getEmail().equals(email)) {
+                Log.d("*-*", arrUser.get(j).getEmail());
+                Toast.makeText(DangKyActivity.this, "Email nay da ton tai!!", Toast.LENGTH_SHORT).show();
+                return false;
+           }
+        }
+        return true;
+    }
+
+    private void dangKi(int i) {
+        users.setName(edtName.getText().toString());
+        users.setPhone(Integer.parseInt(edtPhone.getText().toString()));
+        users.setEmail(edtEmail.getText().toString());
+        users.setAddress(edtAddress.getText().toString());
+        users.setPassword(edtPass.getText().toString());
+        users.setId_Users("User" + i);
+        if (edtPass.getText().toString().equals(edtPass1.getText().toString())) {
+            Table_User.child("User" + i).setValue(users);
+            Toast.makeText(getApplicationContext(), "Dang ki thanh cong", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(getApplicationContext(), "Sai mat khau vui long nhap lai", Toast.LENGTH_SHORT).show();
+        }
+        mData.child("MaxID").child("MaxID_Users").setValue(i);
     }
 
     public void setControl() {
@@ -143,7 +190,36 @@ public class DangKyActivity extends AppCompatActivity {
         edtPass1 = (TextInputEditText) findViewById(R.id.edtPassWord1);
     }
 
+    private void loadUser() {
+        mData.child("Users").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Users us = dataSnapshot.getValue(Users.class);
+                arrUser.add(us);
 
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 
     private boolean isEmptyOrNull(String text) {
         if (text != null && !TextUtils.isEmpty(text)) {
@@ -160,7 +236,7 @@ public class DangKyActivity extends AppCompatActivity {
     }
 
     private boolean isValidPassword(String password) {
-        if (password != null && password.length() > 6) {
+        if (password != null && password.length() >= 6) {
             return true;
         }
         return false;
